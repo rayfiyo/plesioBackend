6e351b47a7b89b676ffba83f01b6f1682a3b3aa0
rayfiyo <rayfiyorex@gmail.com>
2023-12-16 21:22:36 +0900

    update getListOfDir to fetchDir, and used API

--- fetchDir/main.go
+++ fetchDir/main.go
@@ -8,53 +8,42 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
-type Images struct {
+type Dirs struct {
 	Path string `json:"path"`
-	Date string `json:"date"`
 }
 
-func fetchImg(tgtDirText string) ([]Images, error) {
-	var imgLen int
-	var imgPath []string
-	var imgDate []string
+func fetchDir(tgtDirText string) ([]Dirs, error) {
+	var dirPath []string
+	var dirLen int
 	tgtDir := os.DirFS(tgtDirText)
 	err := fs.WalkDir(tgtDir, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil && !os.IsPermission(err) { // ignore permission denied errors
 			return err
 		}
-		if strings.Contains(path, ".png") {
-			fileInfo, err := d.Info()
-			if err != nil {
-				return err
-			}
-			imgPath = append(imgPath, path)
-			imgDate = append(imgDate, fileInfo.ModTime().Format("20060102150405"))
-			imgLen++
+		if d.IsDir() == true {
+			dirPath = append(dirPath, path)
+			dirLen++
 		}
 		return nil
 	})
-	imgData := make([]Images, imgLen)
+	dirData := make([]Dirs, dirLen)
 	if err != nil {
-		return imgData, err
+		return dirData, err
 	}
 
-	for i, v := range imgPath {
-		imgData[i].Path = v
-	}
-	for i, v := range imgDate {
-		imgData[i].Date = v
+	for i, v := range dirPath {
+		dirData[i].Path = v
 	}
 
-	return imgData, nil
+	return dirData, nil
 }
 
 func main() {
-	// fetchImage
-	imgs := make([]Images, 1)
-	imgs, err := fetchImg("../")
+	// fetchDir
+	dirs := make([]Dirs, 1)
+	dirs, err := fetchDir("../../")
 	if err != nil {
 		return
 	}
@@ -63,7 +52,7 @@ func main() {
 	handler1 := func(w http.ResponseWriter, r *http.Request) {
 		var buf bytes.Buffer
 		enc := json.NewEncoder(&buf)
-		if err := enc.Encode(&imgs); err != nil {
+		if err := enc.Encode(&dirs); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(buf.String())
